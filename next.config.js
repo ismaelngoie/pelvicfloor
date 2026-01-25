f5e2b5ab43d551d36fc30d74988208cbc0370603
Ismael Ngoie <ismael@ngoie.com>
2026-01-25 02:33:41 -0500

    Update and rename next.config.ts to next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // This allows us to use images from other domains if needed later
+  images: {
+    domains: [],
+  },
+}
+
+module.exports = nextConfig
