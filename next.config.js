42d33b95cbf77a645f897caca50a8345976a2b7d
Ismael Ngoie <ismael@ngoie.com>
2026-01-25 10:51:04 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // <--- This fixes the "out" directory error
+  output: 'export',
   images: {
-    unoptimized: true, // <--- This allows images to show up without a Node server
+    unoptimized: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
