148ff8e6c94d31148f7a5db6048addeb81b4a130
Ismael Ngoie <ismael@ngoie.com>
2026-02-01 18:26:00 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 "use client";
+
 import React from "react";
 import Onboarding from "./onboarding/onboarding";
 
 export default function Home() {
   return (
-    <div className="w-full h-full min-h-0 bg-app-background flex flex-col overflow-hidden">
+    <div className="w-full h-[100dvh] min-h-[100dvh] bg-app-background flex flex-col overflow-hidden">
       <Onboarding />
     </div>
   );
