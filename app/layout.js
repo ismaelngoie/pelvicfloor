60d40c57ca4c9dce1d7ace7c6b307034a3a49281
Ismael Ngoie <ismael@ngoie.com>
2026-02-02 00:36:14 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -109,7 +109,7 @@ export default function RootLayout({ children }) {
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "v8cur93469");
+            })(window, document, "clarity", "script", "vaulq5g8w8");
           `}
         </Script>
 
